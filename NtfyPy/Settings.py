ba7fa9746789d731b99f781e6a4d6bfed229a6fc
Ilyass-Bougati <i.bougati12@gmail.com>
2025-07-12 12:27:37 +0100

    Ignoring extra env variables

--- NtfyPy/Settings.py
+++ NtfyPy/Settings.py
@@ -1,8 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, ConfigDict
 
 class Settings(BaseSettings):
     host: str = "localhost"
     port: int = 80
+    model_config = ConfigDict(extra='ignore')
 
     class Config:
         env_prefix = "NTFY_"
